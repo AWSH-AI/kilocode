af474bae814b8075316bd07315cc7499c38a35e0
Code Adkins <code@awsh.net>
2025-09-23 19:43:29 -0400

    chore: update provider constants and kilocode token base URL

--- src/api/providers/constants.ts
+++ src/api/providers/constants.ts
@@ -1,8 +1,8 @@
 import { Package } from "../../shared/package"
 
 export const DEFAULT_HEADERS = {
-	"HTTP-Referer": "https://awsh.cloud",
-	"X-Title": "AWSH Code",
+	"HTTP-Referer": "https://awsh.net/code",
+	"X-Title": "AWSH CODE",
 	"X-KiloCode-Version": Package.version,
 	"User-Agent": `Kilo-Code/${Package.version}`,
 }
